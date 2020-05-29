af12ffaed9e303db2d43e2a16e350faadab64fbc
martin <martindc@iesgalileo.es>
2020-05-29 09:28:12 +0200

    primer commit - programa sin switch

---
+++ prueba1git/src/prueba1git/Prueba1git.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba1git;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author martin
+ */
+public class Prueba1git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
+        System.out.println("Teclea el nombre de un mes y te diré los días que tiene:");
+        String mes=teclado.nextLine();
+    }
+    
+}
